48933d79da8b46980999d1c264333082248acccb
sunsun1188 <eunsun08110811@gmail.com>
2023-10-12 15:50:05 +0900

    실습2

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
-int print_star();
+void func1(void) {
+	int x;
+	printf("func1 x is at %p\n",&x); // %p 출력이 16진수 형태로 나옴 
+} 
 
 int main(void) {
+	int x;
+	printf("main x is at %p\n",&x); // x의 주소값 16진수로 출력 
+	func1();
 	
-	print_star();
-	print_star();
-	print_star();
-	
-	return 0;	
-}
-
-void print_star() {   // 출력값 없으므로 void 씀 
-	
-	int i;
-	
-	for (i=0;i<10;i++) {
-		printf("*");
-	}
-}
+	return 0;
+} 
+/* 스태 국조에서 main 함수의 x가 더 아래 있음(메모리 수 더 높음)
+func1 함수의 x 메모리 수 더 낮음 */ 
