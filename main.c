bc5ba3b95ef5195e0947dc2a98f56e5cc49a8c88
sunsun1188 <eunsun08110811@gmail.com>
2023-10-12 16:14:21 +0900

    실습3

--- main.c
+++ main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
-void func1(void) {
-	int x;
-	printf("func1 x is at %p\n",&x); // %p 출력이 16진수 형태로 나옴 
-} 
+int sumTwo(int a, int b) {
+	return a+b;
+}
+
+int square(int n) {
+	return n*n;
+}
+
+int get_max(int x, int y) {
+	if (x>y) 
+		return x;
+	else
+		return y;   // else 뺴도 됨 
+}
 
 int main(void) {
-	int x;
-	printf("main x is at %p\n",&x); // x의 주소값 16진수로 출력 
-	func1();
+	
+	int a,b;
+	
+	a=3;
+	b=5;
+	
+	printf("%d\n",sumTwo(a,b));
+	printf("%d\n",square(a));
+	printf("%d\n",get_max(a,b));
 	
 	return 0;
-} 
-/* 스태 국조에서 main 함수의 x가 더 아래 있음(메모리 수 더 높음)
-func1 함수의 x 메모리 수 더 낮음 */ 
+}
