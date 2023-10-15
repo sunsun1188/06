cb7f9eacb424a40c5ff4d6cb286ca7589836adcc
sunsun1188 <eunsun08110811@gmail.com>
2023-10-15 23:59:40 +0900

    실습5

--- main.c
+++ main.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int square(int a) {
-	return (a*a);
-}
+int combination(int n, int r);
+int factorial(int n);
+void get_integer(int *n, int *r);
 
-int main () {
+int main(int argc, char *argv[]) {
 	
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
+	int n,r;
+	get_integer(&n,&r);
+	
+	int result=combination(n,r);
+	printf("Combination(%d,%d) is %d\n", n,r,result);
+	return 0;
 }
+
+
+int combination(int n, int r) {
+	int div1, div2; // di1 분자 div2  분모
+	div1=factorial(n); //n!
+	div2=factorial(n-r)*factorial(r);
+	return (div1/div2);
+}
+
+int factorial(int n) {
+	
+    int res = 1;
+    for (int i = 1; i<=n; i++) {
+        res *= i;
+    }
+    return res;
+}
+
+void get_integer(int *n, int *r) {
+	
+	printf("input n: ");
+	scanf("%d",n);
+	printf("input r: ");
+	scanf("%d",r);
+}
+
+
